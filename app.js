7e0aa26d725f17fa3b5abd910c252ce333db76ec
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-10 11:21:45 -0500

    Updated initial js solution

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const num2 = document.querySelector("#num2");
 const btn = document.querySelector("button");
 
 const add = (num1, num2) => {
-    return parseInt(num1.value) + parseInt(num2.value);
+    return (typeof num1 === "number" && typeof num2 === "number") 
+            ?  num1.value + num2.value
+            :  (+num1.value) + (+num2.value)
 }
 
 btn.addEventListener("click", () => {
     alert(add(num1, num2));
+    num1.value = '';
+    num2.value = '';
 });
